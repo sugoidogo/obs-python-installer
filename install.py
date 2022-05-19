58c05b711ba93e216a791c0a4c757be55728ad07
josephsmendoza <josephsmendoza@gmail.com>
2022-05-19 01:32:53 -0600

    support existing python installs and use python installer instead of embedded

--- install.py
+++ install.py
@@ -1,53 +1,52 @@
 from os import environ
-from zipfile import ZipFile
-from io import BytesIO
-from urllib.request import urlopen
-from os import makedirs,chdir
+from winreg import *
+from urllib.request import urlretrieve
 from subprocess import run
-from json import load, dump
-from os import listdir
 
-python_url='https://www.python.org/ftp/python/3.6.8/python-3.6.8-embed-amd64.zip'
-install_path=environ['appdata']+'/obs-studio/python/'
-pth_path='python36._pth'
-pth_string='python36.zip\n.\nimport site\n./Lib/site-packages/'
-pip_url='https://bootstrap.pypa.io/pip/3.6/get-pip.py'
-pip_path='get-pip.py'
-pip_command='python.exe get-pip.py --no-warn-script-location'
+python_install_key=R'SOFTWARE\Python\PythonCore\3.6\InstallPath'
+python_url_64='https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe'
+python_url_32='https://www.python.org/ftp/python/3.6.8/python-3.6.8.exe'
+python_installer_args=' /passive Include_launcher=0 AssociateFiles=0 Shortcuts=0'
+is64='64' in environ['PROCESSOR_ARCHITECTURE'] or '64' in environ['PROCESSOR_ARCHITEW6432']
 global_ini_path=environ['appdata']+'/obs-studio/global.ini'
+install_path=None
 
-input('Make sure OBS is closed, then press Enter to continue')
-print('downloading python...')
-zip=ZipFile(BytesIO(urlopen(python_url).read()))
-print('creating install folder...')
-makedirs(install_path,exist_ok=True)
-print('extracting python...')
-chdir(install_path)
-for filename in zip.namelist():
-    ofile=open(filename,'wb')
-    ifile=zip.open(filename)
-    ofile.write(ifile.read())
-    ofile.close()
-    ifile.close()
-print('enabling site-packages...')
-pth=open(pth_path,'w')
-pth.write(pth_string)
-pth.close()
-print('downloading pip installer...')
-get_pip=open(pip_path,'wb')
-get_pip.write(urlopen(pip_url).read())
-get_pip.close()
-print('running pip installer...')
-run(install_path+pip_command)
-print('setting python path in OBS...')
-global_ini_file=open(global_ini_path,'a')
-global_ini_file.writelines([
-    '\n',
-    '[Python]\n'
-    'Path64bit='+install_path+'\n'
-])
-global_ini_file.close()
-print('')
-print('Python 3.6.8 embedded plus pip has been installed')
-print('Your OBS Settings have been updated')
-input('You may now close this window')
+def ensureInstallPath()->str:
+    print('Searching for conflicting python installs')
+    try:
+        install_path=QueryValue(HKEY_LOCAL_MACHINE, python_install_key)
+        print('System python 3.6 found')
+        return install_path
+    except:
+        print('System python 3.6 not found')
+    try:
+        install_path=QueryValue(HKEY_CURRENT_USER, python_install_key)
+        print('User python 3.6 found')
+        return install_path
+    except:
+        print('User python 3.6 not found')
+    print('downloading python')
+    python_installer=None
+    if is64:
+        python_installer=urlretrieve(python_url_64)[0]
+    else:
+        python_installer=urlretrieve(python_url_32)[0]
+    print('Installing python')
+    run(python_installer+python_installer_args)
+    return QueryValue(HKEY_CURRENT_USER, python_install_key)
+
+def main():
+    input('Make sure OBS is closed, then press enter to continue.')
+    install_path=ensureInstallPath()
+    print('Updating OBS config')
+    global_ini_file=open(global_ini_path,'a')
+    global_ini_file.writelines([
+        '\n',
+        '[Python]\n'
+        'Path64bit='+install_path+'\n'
+    ])
+    global_ini_file.close()
+    input('\nOBS can now use python 3.6 scripts\nYou can close this window\nor press enter to exit')
+
+if __name__ == '__main__':
+    main()
